5c73686efcf5260604515eba3236b558782b5388
BhaktiHirani <hiranibhakti818@gmail.com>
2024-12-11 09:00:45 +0530

    instructor profile screen done

--- lib/Screens/InstructorPanel/InstructorProfile_screen.dart
+++ lib/Screens/InstructorPanel/InstructorProfile_screen.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterpro/Screens/authentication/InstructorEditProfile_Screen.dart';
-
 import '../../Custom_Widgets/GradientButton.dart';
 import 'Earning&Payment_Screen.dart';
 
@@ -12,48 +11,53 @@ class InstructorProfilePage extends StatefulWidget {
 }
 
 class _InstructorProfilePageState extends State<InstructorProfilePage> {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
   String fullName = '';
   String email = '';
   String expertise = '';
   String about = '';
   String profileImageUrl = 'https://via.placeholder.com/150'; // Default image URL
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    _fetchUserData();
-  }
+
+  bool isLoading = true;
 
   @override
   void initState() {
     super.initState();
     _fetchUserData();
   }
+
   Future<void> _fetchUserData() async {
     try {
       String? uid = _auth.currentUser?.uid;
       if (uid != null) {
         DocumentSnapshot userDoc = await _firestore.collection('users').doc(uid).get();
+
         if (userDoc.exists) {
+          Map<String, dynamic> data = userDoc.data() as Map<String, dynamic>;
+
           setState(() {
-            fullName = userDoc['fullName'] ?? 'John Doe';
-            email = userDoc['email'] ?? 'john@gmail.com';
-            expertise = userDoc['expertise'] ?? 'Expert in Technology & Design';
-            about = userDoc['about'] ?? 'No bio available.';
-            profileImageUrl = userDoc['profileImage'] ?? profileImageUrl;
+            fullName = data['fullName'] ?? 'John Doe';
+            email = data['email'] ?? 'john@gmail.com';
+            expertise = data['expertise'] ?? 'Expert in Technology & Design';
+            about = data['about'] ?? 'No bio available.';
+            profileImageUrl = data['profileImage'] ?? profileImageUrl;
+            isLoading = false;
           });
         } else {
           print("User document does not exist!");
+          setState(() => isLoading = false);
         }
       } else {
         print("User is not logged in!");
+        setState(() => isLoading = false);
       }
     } catch (e) {
       print("Error fetching user data: $e");
+      setState(() => isLoading = false);
     }
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -66,200 +70,107 @@ class _InstructorProfilePageState extends State<InstructorProfilePage> {
               Navigator.push(context, MaterialPageRoute(builder: (context) => EarningsAndPaymentScreen()));
             },
             icon: const Icon(Icons.attach_money_sharp),
-          )
+          ),
         ],
       ),
-      body:RefreshIndicator(
-    onRefresh: _fetchUserData,
-          child:  SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // Profile Header
-            Container(
-              padding: const EdgeInsets.all(16),
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [Colors.blueAccent, Colors.lightBlueAccent],
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                ),
-              ),
-              child: Column(
-                children: [
-                  CircleAvatar(
-                    radius: 60,
-                    backgroundImage: NetworkImage(profileImageUrl), // Display profile image from Firestore
-                  ),
-                  const SizedBox(height: 10),
-                  Text(
-                    fullName,
-                    style: const TextStyle(
-                      fontSize: 22,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                  const SizedBox(height: 4),
-                  Text(
-                    expertise,
-                    style: const TextStyle(
-                      fontSize: 16,
-                      color: Colors.white70,
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Icon(Icons.check_circle, color: Colors.greenAccent),
-                      SizedBox(width: 5),
-                      Text(
-                        'Verified Instructor',
-                        style: TextStyle(color: Colors.white),
+      body: isLoading
+          ? Center(child: CircularProgressIndicator())
+          : RefreshIndicator(
+              onRefresh: _fetchUserData,
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // Profile Header
+                    Container(
+                      padding: const EdgeInsets.all(16),
+                      decoration: const BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [Colors.blueAccent, Colors.lightBlueAccent],
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                        ),
                       ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-
-            // About Section
-            Padding(
-              padding: const EdgeInsets.all(16),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    'About',
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
+                      child: Column(
+                        children: [
+                          CircleAvatar(
+                            radius: 60,
+                            backgroundImage: NetworkImage(profileImageUrl),
+                          ),
+                          const SizedBox(height: 10),
+                          Text(
+                            fullName,
+                            style: const TextStyle(
+                              fontSize: 22,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                          const SizedBox(height: 4),
+                          Text(
+                            expertise,
+                            style: const TextStyle(
+                              fontSize: 16,
+                              color: Colors.white70,
+                            ),
+                          ),
+                          const SizedBox(height: 8),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: const [
+                              Icon(Icons.check_circle, color: Colors.greenAccent),
+                              SizedBox(width: 5),
+                              Text(
+                                'Verified Instructor',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 10),
-                  Text(
-                    about,
-                    style: const TextStyle(fontSize: 16, color: Colors.grey),
-                  ),
-                ],
-              ),
-            ),
-
-
-            const SizedBox(height: 16),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: GradientButton(
-                buttonText: 'Edit Profile',
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => InstructorEditProfileScreen()),
-                  ).then((_){
-                    _fetchUserData();
-                  });
-                },
-                gradientColors: [Colors.blue, Colors.blueAccent],
-              ),
-            ),
-            const SizedBox(height: 16),
-
-            // Courses Section
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: const Text(
-                'Courses',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
+                    // About Section
+                    Padding(
+                      padding: const EdgeInsets.all(16),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            'About',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          Text(
+                            about,
+                            style: const TextStyle(fontSize: 16, color: Colors.grey),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16),
+                      child: GradientButton(
+                        buttonText: 'Edit Profile',
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => InstructorEditProfileScreen()),
+                          ).then((_) {
+                            _fetchUserData();
+                          });
+                        },
+                        gradientColors: [Colors.blue, Colors.blueAccent],
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                  ],
                 ),
               ),
             ),
-            const SizedBox(height: 10),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SizedBox(
-                height: 210, // Adjust height as needed
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: 5, // Replace with dynamic data
-                  itemBuilder: (context, index) {
-                    return _buildCourseCard();
-                  },
-                ),
-              ),
-            ),
-            SizedBox(height: 20,)
-          ],
-        ),
-      ),
-    ));
-  }
-
-
-  // Course Card Widget
-  Widget _buildCourseCard() {
-    return Container(
-      width: 150, // Adjust width as needed
-      margin: const EdgeInsets.symmetric(horizontal: 8),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(12),
-        color: Colors.white,
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.3),
-            blurRadius: 5,
-            spreadRadius: 2,
-            offset: const Offset(0, 3),
-          ),
-        ],
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            height: 100,
-            decoration: BoxDecoration(
-              borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(12),
-                topRight: Radius.circular(12),
-              ),
-              image: DecorationImage(
-                image: NetworkImage(
-                  'https://via.placeholder.com/150', // Replace with course image
-                ),
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: const [
-                Text(
-                  'Course Title',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                SizedBox(height: 4),
-                Text(
-                  '4.5 ★ | 100 Students',
-                  style: TextStyle(
-                    fontSize: 14,
-                    color: Colors.grey,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
